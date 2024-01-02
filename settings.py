c623dfd9c50e15a5da12fe392cad67ec3bbf41e5
potatoii <potatoii@qq.com>
2024-01-02 16:36:04 +0800

    feat: 修改配置文件

--- settings.py
+++ settings.py
@@ -9,17 +9,20 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 
 
 class LogSettings(TomlSettings):
+    __table_name__ = "log"
     log_name: str = "simbot.log"
     stdout_level: str = "INFO"
     file_level: str = "INFO"
 
 
 class UserSettings(TomlSettings):
+    __table_name__ = "user"
     email: str
     password: str
 
 
 class MailSettings(TomlSettings):
+    __table_name__ = "notice.mail"
     host: Optional[str] = Field(required=False, default="smtp.qq.com")
     port: Optional[int] = Field(required=False, default=465)
     username: Optional[str] = Field(required=False, default="")
@@ -27,10 +30,12 @@ class MailSettings(TomlSettings):
 
 
 class BarkSettings(TomlSettings):
+    __table_name__ = "notice.bark"
     bark_key: Optional[str] = Field(required=False, default="")
 
 
 class NoticeSettings(TomlSettings):
+    __table_name__ = "notice"
     mail: MailSettings = MailSettings()
     bark: BarkSettings = BarkSettings()
 
--- toml_settings.py
+++ toml_settings.py
@@ -1,5 +1,5 @@
 import tomllib
-from typing import Any
+from typing import Any, Optional
 
 from pydantic.fields import FieldInfo
 from pydantic_settings import (
@@ -15,6 +15,12 @@ class TomlSettings(BaseSettings):
         env_file="config.local.toml",
         extra="ignore"
     )
+
+    def __init__(self, env_file: str = None):
+        if env_file:
+            self.model_config["env_file"] = env_file
+
+        super().__init__()
 
     @classmethod
     def settings_customise_sources(
@@ -38,6 +44,11 @@ class TomlConfigSettingsSource(PydanticBaseSettingsSource):
         self.config = settings_cls.model_config
         self.dotenv_settings = dotenv_settings
 
+    def get_table_name(self) -> Optional[str]:
+        if hasattr(self.settings_cls, "__table_name__"):
+            return getattr(self.settings_cls, "__table_name__")
+        return None
+
     def get_nested_value(self, dictionary, key):
         if isinstance(dictionary, dict):
             if key in dictionary:
@@ -50,15 +61,25 @@ class TomlConfigSettingsSource(PydanticBaseSettingsSource):
         return None
 
     def get_field_value(self, field: FieldInfo, field_name: str) -> tuple[Any, str, bool]:
+        table_name = self.get_table_name()
         env_file = self.config.get("env_file")
+
         if isinstance(self.dotenv_settings, DotEnvSettingsSource):
             env_file = env_file if env_file else self.dotenv_settings.env_file
+
         if not env_file:
             raise ValueError("No toml env_file")
+
         try:
             with open(env_file, "rb") as ft:
                 file_content_toml = tomllib.load(ft)
-                field_value = self.get_nested_value(file_content_toml, field_name)
+
+                if table_name:
+                    table_content = file_content_toml.get(table_name, {})
+                    field_value = table_content.get(field_name)
+                else:
+                    field_value = self.get_nested_value(file_content_toml, field_name)
+
                 return field_value, field_name, False
         except Exception as e:
             raise ValueError(f"Error on open f{env_file}: {e}") from e
