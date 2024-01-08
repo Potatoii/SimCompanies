87fbd6eb58a190fd619291b429a93d8c248b1fa2
potatoii <potatoii@qq.com>
2024-01-08 21:22:56 +0800

    feat: Bark通知添加公司名

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from log_utils import logger
 from notification import notifier
 from sim_request import SimClient
 from utils import get_building_info, BuildingInfo
+from utils.api_utils import get_my_company
 
 
 async def building_status_monitor():
@@ -14,6 +15,8 @@ async def building_status_monitor():
     """
     idle_building_set = set()
     async with SimClient() as simclient:
+        my_company = await get_my_company(simclient=simclient)
+        company_name = my_company.authCompany.company
         while True:
             building_dict: Dict[int, BuildingInfo] = await get_building_info(simclient=simclient)
             for building_id, building_info in building_dict.items():
@@ -21,7 +24,7 @@ async def building_status_monitor():
                 is_idle = building_id in idle_building_set
                 if building_info.status == "idle":
                     if not is_idle:
-                        logger.info(f"{building_name}已空闲")
+                        logger.info(f"{company_name}-{building_name}已空闲")
                         await notifier.notify(f"{building_name}已空闲")
                         idle_building_set.add(building_id)
                 elif is_idle:
